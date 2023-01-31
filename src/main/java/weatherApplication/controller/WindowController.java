245b10e164943aa1909603eeac860fc76dd4379a
Mateusz Wajnberger <mateusz.wajnberger@gmail.com>
2023-02-01 00:36:16 +0100

    add WindowController skeleton from SceneBuilder

--- src/main/java/weatherApplication/controller/WindowController.java
+++ src/main/java/weatherApplication/controller/WindowController.java
@@ -1,4 +1,40 @@
 package weatherApplication.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class WindowController {
+
+    @FXML
+    private Button currentLocationButton;
+
+    @FXML
+    private TextField currentLocationTextField;
+
+    @FXML
+    private Button destinationButton;
+
+    @FXML
+    private TextField destinationTextField;
+
+    @FXML
+    void currentLocationButtonAction() {
+
+    }
+
+    @FXML
+    void currentLocationTextFieldAction() {
+
+    }
+
+    @FXML
+    void destinationButtonAction() {
+
+    }
+
+    @FXML
+    void destinationTextFieldAction() {
+
+    }
 }
