3572990b99bdc166bb55ff0b55df7c22be57dede
Mateusz Wajnberger <mateusz.wajnberger@gmail.com>
2023-02-24 02:27:06 +0100

    test WeatherService

---
+++ src/test/java/weatherApplication/model/WeatherServiceTest.java
@@ -0,0 +1,63 @@
+package weatherApplication.model;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import weatherApplication.model.client.WeatherClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class WeatherServiceTest {
+
+    @Mock
+    private WeatherClient weatherClient = mock(WeatherClient.class);
+
+    @InjectMocks
+    private WeatherService weatherService = new WeatherService(weatherClient);
+
+    @Test
+    void shouldReturnWeather() throws IOException {
+        //given
+        Weather expectedWeather = new Weather(prepareWeatherData());
+        given(weatherClient.getWeather("Berlin")).willReturn(expectedWeather);
+
+        //when
+        Weather result = weatherService.getWeather("Berlin");
+
+        //then
+        assertThat(result, equalTo(expectedWeather));
+    }
+
+    @Test
+    void shouldNotReturnWeatherIfTwoDifferentCities() throws IOException {
+        //given
+        Weather expectedWeather = new Weather(prepareWeatherData());
+        given(weatherClient.getWeather("Berlin")).willReturn(expectedWeather);
+
+        //when
+        Weather result = weatherService.getWeather("Cracow");
+
+        //then
+        assertThat(result, not(equalTo(expectedWeather)));
+    }
+
+    private List<WeatherParameters> prepareWeatherData() {
+        List<WeatherParameters> weatherData = new ArrayList<>();
+
+        weatherData.add(new WeatherParameters("Today", "02d", "15°C", "1015hPa", "3m/s" ));
+        WeatherParameters weatherParameters =
+                new WeatherParameters("Tuesday", "04d", "10°C", "1013hPa", "2m/s" );
+        for (int i = 0; i < 4; i++) {
+            weatherData.add(weatherParameters);
+        }
+        return weatherData;
+    }
+}
